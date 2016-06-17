d3b3bd8ec01a678bd7fed41c209b99a307084b7c
Leran Firer <leranf@gmail.com>
2016-06-17 11:30:50 -0700

    basic frontend skeleton

---
+++ components/app.js
@@ -0,0 +1,14 @@
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
