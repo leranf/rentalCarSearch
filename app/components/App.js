6acb3f5afa936bb3cd7a946b5ef288f5f42d9812
Leran Firer <leranf@gmail.com>
2016-06-21 10:35:42 -0700

    places implicit component in app

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Search from './Search';
 const injectTapEventPlugin = require('react-tap-event-plugin');
 injectTapEventPlugin();
 
@@ -8,8 +9,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
-        {this.props.children}
+        <Header />
+        <Search />
         <Footer/>
       </div>
     );
